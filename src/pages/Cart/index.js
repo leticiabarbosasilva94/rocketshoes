3c2ae7ba17dadeb3190d8fd634abeadb1d95524b
Letícia Barbosa Silva <leticiabarbosasilva94@gmail.com>
2019-12-07 10:09:55 -0300

    Cart styled

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,5 +1,65 @@
 import React from 'react';
+import {
+  MdAddCircleOutline,
+  MdDelete,
+  MdRemoveCircleOutline
+} from 'react-icons/md';
+
+import { Container, Total, ProductTable } from './styled';
 
 export default function Cart() {
-  return <h1>Cart</h1>;
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th>&nbsp;</th>
+            <th>PRODUTO</th>
+            <th>QTD</th>
+            <th>SUBTOTAL</th>
+            <th>&nbsp;</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img src="https://picsum.photos/id/532/800/800" alt="pic" />
+            </td>
+            <td>
+              <strong>Tenis legal</strong>
+              <span>R$129,90</span>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                </button>
+                <input type="number" readOnly value={1} />
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="#7159c1" />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>R$258,80</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+
+      <footer>
+        <button type="button">Finalizar Pedido</button>
+
+        <Total>
+          <span>TOTAL</span>
+          <strong>R$1920,20</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
 }
--- src/pages/Cart/styled.js
+++ src/pages/Cart/styled.js
@@ -1,3 +1,95 @@
 import styled from 'styled-components';
 
-export const Temp = styled.h1``;
+export const Container = styled.div`
+  padding: 30px;
+  background: #fff;
+  border-radius: 4px;
+
+  footer {
+    margin-top: 30px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    button {
+      background: #7150c1;
+      border: none;
+      color: #fff;
+      border-radius: 4px;
+      padding: 12px 20px;
+      text-transform: uppercase;
+      transition: filter 600ms;
+      font-weight: bold;
+
+      &:hover {
+        filter: brightness(80%);
+      }
+    }
+  }
+`;
+export const ProductTable = styled.table`
+  width: 100%;
+
+  th {
+    color: #999;
+    text-align: left;
+    padding: 12px;
+  }
+
+  td {
+    padding: 12px;
+    border-bottom: 1px solid #eee;
+  }
+
+  img {
+    height: 100px;
+  }
+
+  strong {
+    color: #333;
+    display: block;
+  }
+
+  span {
+    margin-top: 5px;
+    font-size: 18px;
+    font-weight: bold;
+    display: block;
+  }
+
+  div {
+    display: flex;
+    align-items: center;
+
+    input {
+      border: 1px solid #ddd;
+      border-radius: 4px;
+      color: #666;
+      padding: 6px;
+      width: 50px;
+    }
+  }
+
+  button {
+    background: none;
+    padding: 6px;
+    border: none;
+    color: #7150c1;
+  }
+`;
+export const Total = styled.div`
+  display: flex;
+  align-items: baseline;
+
+  span {
+    color: #999;
+    font-weight: bold;
+    display: block;
+    margin-right: 10px;
+  }
+
+  strong {
+    font-size: 28px;
+    margin-left: 5px;
+  }
+`;
